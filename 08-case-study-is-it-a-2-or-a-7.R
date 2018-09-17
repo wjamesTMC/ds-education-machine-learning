eeaa2410f91b53cb52c9a86a0b4ca2b13f094a2e
wjamesTMC <jamesw@csps.com>
2018-09-17 07:17:47 -0400

    Updates and cleanup

--- 08-case-study-is-it-a-2-or-a-7.R
+++ 08-case-study-is-it-a-2-or-a-7.R
@@ -14,7 +14,16 @@ library(HistData)
 library(caret)
 library(e1071)
 
-# In the two simple examples above, we only had one predictor. We actually do not consider these machine learning challenges, which are characterized by cases with many predictors. Let’s go back to the digits example in which we had 784 predictors. For illustrative purposes, we will start by simplifying this problem to one with 2 predictors and two classes. Specifically, we define the challenge as building an algorithm that can determine if a digit is a 2 or 7 from the predictors. We are not quite ready to build algorithms with 784 predictors so we will extract two simple predictors from the 784: the proportion of dark pixels that are in the upper left quadrant (X1) and the lower right quadrant (X2).
+# In the two simple examples above, we only had one predictor. We actually do
+# not consider these machine learning challenges, which are characterized by
+# cases with many predictors. Let’s go back to the digits example in which we
+# had 784 predictors. For illustrative purposes, we will start by simplifying
+# this problem to one with 2 predictors and two classes. Specifically, we define
+# the challenge as building an algorithm that can determine if a digit is a 2 or
+# 7 from the predictors. We are not quite ready to build algorithms with 784
+# predictors so we will extract two simple predictors from the 784: the
+# proportion of dark pixels that are in the upper left quadrant (X1) and the
+# lower right quadrant (X2).
 
 # We then select a random sample of 1,000 digits, 500 in the training set and
 # 500 in the test set and provide them here:
@@ -26,6 +35,8 @@ data("mnist_27")
      
 mnist_27$train %>% ggplot(aes(x_1, x_2, color = y)) +
 geom_point()
+
+# IMAGE
 
 # We can immediately see some patterns. For example, if X1 (the upper left
 # panel) is very large, then the digit is probably a 7. Also, for smaller values
@@ -105,6 +116,8 @@ geom_raster() +
 scale_fill_gradientn(colors=c("#F8766D","white","#00BFC4")) +
 stat_contour(breaks=c(0.5),color="black")
 
+# IMAGE
+
 # So above you see a plot of the true P(X,Y). To start understanding the
 # limitations of logistic regression here, first, note that with logistic
 # regression P(X,Y) has to be a plane and as a result the boundary defined by
@@ -127,7 +140,10 @@ mnist_27$true_p %>% mutate(p_hat = p_hat) %>%
      scale_fill_gradientn(colors=c("#F8766D","white","#00BFC4")) +
      stat_contour(breaks=c(0.5),color="black") 
 
-# We can see where the mistakes were made mainly come from low values X1 that have either high or low value of X2. Logistic regression can’t catch this.
+# IMAGE
+
+# We can see where the mistakes were made mainly come from low values X1 that
+# have either high or low value of X2. Logistic regression can’t catch this.
 
 p_hat <- predict(fit, newdata = mnist_27$true_p)
 mnist_27$true_p %>% mutate(p_hat = p_hat) %>%
@@ -135,6 +151,14 @@ mnist_27$true_p %>% mutate(p_hat = p_hat) %>%
      stat_contour(aes(x_1, x_2, z=p_hat), breaks=c(0.5), color="black") + 
      geom_point(mapping = aes(x_1, x_2, color=y), data = mnist_27$test) 
 
-# We need something more flexible. A method that permits estimates with shapes other than a plane.
+# IMAGE
 
-# We are going to learn a few new algorithms based on different ideas and concepts. But what they all have in common is that they permit more flexible approaches. We will start by describing nearest neighbor and kernel approaches. To introduce the concepts behinds these approaches, we will again start with a simple one dimensional example and describe the concept of smoothing.
+# We need something more flexible. A method that permits estimates with shapes
+# other than a plane.
+
+# We are going to learn a few new algorithms based on different ideas and
+# concepts. But what they all have in common is that they permit more flexible
+# approaches. We will start by describing nearest neighbor and kernel
+# approaches. To introduce the concepts behinds these approaches, we will again
+# start with a simple one dimensional example and describe the concept of
+# smoothing.
