fb039828b6eb71886b884a762f47959a2838ed1c
wjamesTMC <jamesw@csps.com>
2018-10-29 16:35:28 -0400

    mnist updates

--- 01-mnist().R
+++ 01-mnist().R
@@ -4,6 +4,14 @@
 #
 # --------------------------------------------------------------------------------
 
+# A list with four items: Xtrain is a training set matrix with 6000 rows
+# (samples) and 784 columns (features), Xtrain is an integer array of
+# corresponding training class labels, Xtest is a test set matrix of 10000 rows
+# and 784 columns, and Ytest is the corresponding class labels. Rows in Xtrain
+# and Xtest correspond to different images of digits, and columns correspond to
+# the pixel intensities in each image, obtained by flattening the image pixels
+# in column-major ordering.
+
 # Setup
 library(tidyverse)
 library(dslabs)
