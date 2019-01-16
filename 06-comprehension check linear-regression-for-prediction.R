6d4e6ae93b74750cfb4ae3c674090462e76ee544
wjamesTMC <jamesw@csps.com>
2019-01-16 13:23:04 -0500

    First commit...

--- 06-comprehension check linear-regression-for-prediction.R
+++ 06-comprehension check linear-regression-for-prediction.R
@@ -23,19 +23,44 @@ dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
      data.frame() %>% setNames(c("x", "y"))
 
 # Use the caret package to partition the dataset into test and training sets of
-# equal size. Train a linear model and calculate the RMSE. Repeat this exercise
-# 100 times and report the mean and standard deviation of the RMSEs. (Hint: You
-# can use the code shown in a previous course inside a call to replicate using a
-# seed of 1).
+# equal size. 
 
 test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
 
 train_set <- dat %>% slice(-test_index)
 test_set <- dat %>% slice(test_index)
 
+# Train a linear model and calculate the RMSE. 
+lm_fit <- mutate(train_set, y = as.numeric(sex == "Female")) %>% lm(y ~ height, data = .)
+
+The dependent variable is y and predictor is height.
+
+
+Once lm_fit is found I was expecting y_hat will be calculated using the lm_fit like this:
+     
+     y_hat <- predict(lm_fit, test_set)
+
+Instead, he calculated y_hat using the model and then calculated y_hat from p_hat. 
+
+
+p_hat <- predict(lm_fit, test_set)
+
+# The above code simply produced object "p_hat", which houses all of the
+# "prediction" values.
+
+y_hat <- ifelse(p_hat > 0.5, "Female", "Male") %>% factor()
+
+# Now that we have all of our prediction values (Female or Male "dummified" into
+# 0 or 1), the above code tells your machine to ultimately predict "Female" if
+# p_hat (all predictions) is greater than 0.5. If you remember, Female = 1 and
+# Male = 0. The last bit just makes y_hat a factor.
+
 mean(RMSE) : two decimal places are sufficient
 sd(RMSE): for a correct answer three decimal places are necessary.
 
+# Repeat this exercise 100 times and report the mean and standard deviation of
+# the RMSEs. (Hint: You can use the code shown in a previous course inside a
+# call to replicate using a seed of 1).
 
 
 
