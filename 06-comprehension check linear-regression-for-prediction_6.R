6247019e6fe8f1acf52730ab22d6c1ac2b5df5d6
wjamesTMC <jamesw@csps.com>
2019-01-18 15:40:53 -0500

    Part way through the problem

--- 06-comprehension check linear-regression-for-prediction_6.R
+++ 06-comprehension check linear-regression-for-prediction_6.R
@@ -31,7 +31,15 @@ dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
 
 # Which of the three models performs the best (has the lowest RMSE)?
      
-# Replication
+# -----------------------------------------------------------------
+# x_1
+# -----------------------------------------------------------------
+set.seed(1)
+n <- 1000
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.25, 0.75, 0.25, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+     data.frame() %>% setNames(c("y", "x_1", "x_2"))
+
 set.seed(1)
 results <- replicate(100, expr = {
      
@@ -41,7 +49,7 @@ results <- replicate(100, expr = {
      test_set <- dat[train_index,] 
      
      # Train the model
-     model <- lm(y ~ x, data = train_set)
+     model <- lm(y ~ x_1, data = train_set)
      model_prediction <- predict(model, test_set) 
      
      # Calculate the RMSE
@@ -49,16 +57,77 @@ results <- replicate(100, expr = {
      model_rmse <- sqrt(mean(model_result^2))
 })
 mean(results) # two decimal places are sufficient
-# [1] 2.488661 (for c(1.0, 0.5, 0.5, 1.0))
-# [1] 1.888025 (for c(1.0, 0.75, 0.75, 1.0))
-# [1] 0.9099808 (for c(1.0, 0.95, 0.95, 1.0))
-# [1] 0.4097947 (for c(1.0, 0.99, 0.99, 1.0))
-sd(results)   # for a correct answer three decimal places are necessary
-# [1] 0.1243952 (for c(1.0, 0.5, 0.5, 1.0))
-# [1] 0.1041058 (for c(1.0, 0.5, 0.5, 1.0))
-# [1] 0.06244347 (for c(1.0, 0.95, 0.95, 1.0))
-# [1] 0.02782272 (for c(1.0, 0.99, 0.99, 1.0))
+# [1] 0.6369011
+sd(results) # three decimal places
+# [1] 0.04133126
 
-# Answer: When we increase the correlation between x and y, x has more
-# predictive power and thus provides a better estimate of y
+# -----------------------------------------------------------------
+# x_2
+# -----------------------------------------------------------------
+set.seed(1)
+n <- 1000
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.25, 0.75, 0.25, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+     data.frame() %>% setNames(c("y", "x_1", "x_2"))
+
+set.seed(1)
+results <- replicate(100, expr = {
+     
+     # Partition the dataset into test and training sets of equal size
+     train_index <- createDataPartition(dat$y, p = .5, list = FALSE, times = 1)
+     train_set <- dat[-train_index,]
+     test_set <- dat[train_index,] 
+     
+     # Train the model
+     model <- lm(y ~ x_2, data = train_set)
+     model_prediction <- predict(model, test_set) 
+     
+     # Calculate the RMSE
+     model_result <- test_set$y - model_prediction
+     model_rmse <- sqrt(mean(model_result^2))
+})
+mean(results) # two decimal places are sufficient
+# [1] 0.6515611
+sd(results) # three decimal places
+# [1] 0.03701591
+
+# x_1 and x_2
+set.seed(1)
+n <- 1000
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.25, 0.75, 0.25, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+     data.frame() %>% setNames(c("y", "x_1", "x_2"))
+
+set.seed(1)
+results <- replicate(n, expr = {
+     
+     # Partition the dataset into test and training sets of equal size
+     train_index <- createDataPartition(dat$y, p = .5, list = FALSE, times = 1)
+     train_set <- dat[-train_index,]
+     test_set <- dat[train_index,] 
+     
+     # Train the model
+     model <- lm(y ~ x_1 + x_2, data = train_set)
+     model_prediction <- predict(model, test_set) 
+     
+     # Calculate the RMSE
+     model_result <- test_set$y - model_prediction
+     model_rmse <- sqrt(mean(model_result^2))
+})
+mean(results) # two decimal places are sufficient
+# [1] 0.3450927
+sd(results)   # for a correct answer three decimal places are necessary
+# [1] 0.02475961
+
+# Comparisons
+# x_1
+# [1] 0.6369011
+# [1] 0.04133126
+# x_2
+# [1] 0.6515611
+# [1] 0.03701591
+# Both *** INCORRECT ***
+# [1] 0.3450927
+# [1] 0.02475961
+
 
