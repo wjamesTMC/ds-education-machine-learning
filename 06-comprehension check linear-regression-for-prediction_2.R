8dbd1269b6f6b1c7abfb12103f8f5f436197b3dd
wjamesTMC <jamesw@csps.com>
2019-01-18 13:11:57 -0500

    Question 2 completed

--- 06-comprehension check linear-regression-for-prediction_2.R
+++ 06-comprehension check linear-regression-for-prediction_2.R
@@ -14,44 +14,11 @@ library(HistData)
 library(caret)
 library(e1071)
 
-# Previous exercise
-set.seed(1)
-n <- 100
-Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
-dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>% 
-     data.frame() %>% setNames(c("x", "y"))
-
-# Replication
-set.seed(1)
-results <- replicate(100, expr = {
-     
-     # Partition the dataset into test and training sets of equal size
-     train_index <- createDataPartition(dat$y, p = .5, list = FALSE, times = 1)
-     train_set <- dat[-train_index,]
-     test_set <- dat[train_index,] 
-     
-     # Train the model
-     model <- lm(y ~ x, data = train_set)
-     model_prediction <- predict(model, test_set) 
-     
-     # Calculate the RMSE
-     model_result <- test_set$y - model_prediction
-     model_rmse <- sqrt(mean(model_result^2))
-})
-mean(results) # two decimal places are sufficient
-# [1] 2.488661
-sd(results)   # for a correct answer three decimal places are necessary
-# [1] 0.1243952
-
-# *** End of previous exercise ***
-
-# Q2 Exercise
 # Now we will repeat the above but using larger datasets. Repeat the previous
 # exercise but for datasets with n <- c(100, 500, 1000, 5000, 10000). Save the
 # average and standard deviation of RMSE from the 100 repetitions using a seed
 # of 1. Hint: use the sapply or map functions.
 
-# This code is good
 n <- c(100, 500, 1000, 5000, 10000)
 set.seed(1)
 sapply(n, rmse)
@@ -79,24 +46,16 @@ rmse <- function(n) {
      print(mean(results))
      print(sd(results))
 }
-     
+# [1] 2.497754
+# [1] 0.1180821
+# [1] 2.720951
+# [1] 0.08002108
+# [1] 2.555545
+# [1] 0.04560258
+# [1] 2.624828
+# [1] 0.02309673
+# [1] 2.618442
+# [1] 0.01689205     
 
-mean(results) # two decimal places are sufficient
-# [1] 2.488661
-sd(results)   # for a correct answer three decimal places are necessary
-# [1] 0.1243952
-
-# The vector n <- c(100, 500, 1000, 5000, 10000) is NOT the n for inside
-# replicate. Your call should remain replicate(100, ...). The varying n is
-# actually from the dat assignment:
-#
-# dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>% data.frame() %>%
-# setNames(c("x", "y"))
-#
-# Change n = 100 to regular n, put this data call inside the function you write
-# for sapply/map. Last, make sure your set.seed(1) call is OUTSIDE the function you write. Given a function called rmse with no seed call and vector n, you should be able to get the answer with:
-
-# set.seed(1)
-# sapply(n, rmse)
 
 
